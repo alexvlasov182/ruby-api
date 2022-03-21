a349a01245fdffcc551fe6f0a02313210d8e888a
Alex Vlasov <drumlife182@gmail.com>
2022-03-21 08:35:52 +0100

    api sessions_controller.rb added

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < Devise::SessionsController
   # sign in
   def create
     if @user.valid_password?(sign_in_params[:password])
-      sing_in 'user', @user
+      # sing_in 'user', @user
       json_response 'Signed In Successfuly', true, { user: @user }, :ok
     else
       json_response 'Unauthorized', false, {}, :unauthorized
@@ -17,8 +17,9 @@ class Api::V1::SessionsController < Devise::SessionsController
     params.require(:sign_in).permit(:email, :password)
   end
 
+  # load user
   def load_user
     @user = User.find_for_database_authentication(email: sign_in_params[:email])
-    @user || json_response('Cannot get User', false, {}, :failure)
+    @user || json_response('Cannot get User', false, {}, :failed_dependency)
   end
 end
